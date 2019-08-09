85fe1a5e01c74fc9c16ebc12c351f1782fc3f285
nmcdonough <mcnoah94@gmail.com>
2019-08-09 13:03:06 -0400

    Changed resume file to be returned when clicking on the 'View Mission Results' button. App now tracks how many times the resume is downloaded by reading from/writing to 'hits.txt'.

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const fs = require('fs');
 
 app.get('/', function(req, res){
 	res.sendFile(path.resolve("./index.html"));
 })
 
 app.get('/resume', function(req, res){
-	res.sendFile(path.resolve("./noahres.pdf"));
+	console.log("resume queried!");
+	res.setHeader('Content-disposition', 'attachment; filename=noah_mcdonough.pdf');
+	res.sendFile(path.resolve("./noah_mcdonough.pdf"));
+	fs.readFile("hits.txt", "utf-8", (err, data) => {
+		var hits = parseInt(data, 10);
+  		console.log(data);
+		hits += 1;
+		fs.writeFile("hits.txt", hits, err => {
+			if(err){
+				console.log("Write error:\n" + err);			
+			}		
+		});
+	});
 })
 
 app.use(express.static('public'));
